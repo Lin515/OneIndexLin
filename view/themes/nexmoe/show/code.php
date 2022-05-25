14834f87277b7f04537f6301def86817f702ef22
LinGe <35224561+Lin515@users.noreply.github.com>
2022-05-25 16:19:25 +0800

    稍微清理一些没有用的东西

--- view/themes/nexmoe/show/code.php
+++ view/themes/nexmoe/show/code.php
@@ -1,5 +1,5 @@
 <?php view::layout('layout')?>
-<?php 
+<?php
 	function code_type($ext){
 		$code_type['html'] = 'html';
 		$code_type['htm'] = 'html';
@@ -12,7 +12,7 @@
 		$code_type['txt'] = 'Text';
 		$code_type['sh'] = 'sh';
 		$code_type['md'] = 'Markdown';
-		
+
 		return @$code_type[$ext];
 	}
 	$language = code_type($ext);
@@ -21,7 +21,7 @@
 ?>
 <?php view::begin('content');?>
 <style type="text/css" media="screen">
-    #editor { 
+    #editor {
         /*height:800px;*/
     }
 </style>
@@ -34,19 +34,19 @@
 	        <label class="mdui-textfield-label">下载地址</label>
 	        <input class="mdui-textfield-input" type="text" value="<?php e($url);?>"/>
         </div>
-    
+
     </div>
 </div>
 <a href="<?php e($url);?>" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
 
-<script src="<?php e(statics_cdn()); ?>ace@1.4.13/src/ace.js"></script>
-<script src="<?php e(statics_cdn()); ?>ace@1.4.13/src/ext-language_tools.js"></script>
+<script src="<?php e(statics_cdn()); ?>ace@1.4.13/src-min/ace.js"></script>
+<script src="<?php e(statics_cdn()); ?>ace@1.4.13/src-min/ext-language_tools.js"></script>
 <script>
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/ambiance");
     editor.setFontSize(18);
     editor.session.setMode("ace/mode/<?php e($language);?>");
-    
+
     //Autocompletion
     editor.setOptions({
         enableBasicAutocompletion: true,
